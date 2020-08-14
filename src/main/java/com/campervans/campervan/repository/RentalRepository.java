530e6311950ce030ec2debe250f817bded67837c
Zahari <zahari@softavail.com>
2020-08-14 16:02:36 +0300

    add pagination

--- src/main/java/com/campervans/campervan/repository/RentalRepository.java
+++ src/main/java/com/campervans/campervan/repository/RentalRepository.java
@@ -1,16 +1,24 @@
 package com.campervans.campervan.repository;
 
 import com.campervans.campervan.model.Rental;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
-public interface RentalRepository extends JpaRepository<Rental, Long> {
-    List<Rental> findAll();
+public interface RentalRepository extends  PagingAndSortingRepository<Rental, Long> {
+
+    @RestResource(rel = "rental", path="rental")
+    @Query("SELECT r FROM Rental r WHERE r.name = :name")
+    Page<Rental> findAll(@Param("name") String query, Pageable page);
 
     List<Rental> findByPricePerDayBetween(BigDecimal min, BigDecimal max);
 
